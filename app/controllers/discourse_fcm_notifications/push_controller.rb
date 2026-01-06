1f7ee4c12964d71eb77443e852a0a11180c203fc
Белоногов Антон <62636706+AntonAlekseevich93@users.noreply.github.com>
2026-01-06 18:07:35 +0300

    Update push_controller.rb
    fixes bug with token update

--- app/controllers/discourse_fcm_notifications/push_controller.rb
+++ app/controllers/discourse_fcm_notifications/push_controller.rb
@@ -7,18 +7,27 @@ module ::DiscourseFcmNotifications
     skip_before_action :preload_json
 
     def automatic_subscribe
+      # 1. Если пришел запрос на удаление - удаляем и выходим
       if params[:token] == "REMOVE"
         DiscourseFcmNotifications::Pusher.unsubscribe(current_user)
         render json: { success: 'SUCCESS' }
+        return
+      end
+
+      # 2. Попытка подписки (запись в базу)
+      DiscourseFcmNotifications::Pusher.subscribe(current_user, params[:token])
+
+      # 3. Попытка отправки пуша (игнорируем результат, так как там баг с таймером)
+      DiscourseFcmNotifications::Pusher.confirm_subscribe(current_user)
+
+      # 4. Проверка результата по факту наличия записи в базе
+      # Перезагружаем юзера, чтобы увидеть свежие custom_fields
+      current_user.reload 
+      
+      if current_user.custom_fields[DiscourseFcmNotifications::PLUGIN_NAME] == params[:token]
+        render json: { success: 'SUCCESS' }
       else
-        DiscourseFcmNotifications::Pusher.subscribe(current_user, params[:token])
-        if DiscourseFcmNotifications::Pusher.confirm_subscribe(current_user)
-          #flash.now[:notice] = "You have successfully subscribed to push notifications."
-          render json: { success: 'SUCCESS' }
-        else
-          #flash.now[:alert] = "There was an error subscribing to push notifications."
-          render json: { failed: 'FAILED', error: I18n.t("discourse_fcm_notifications.subscribe_error") }
-        end
+        render json: { failed: 'FAILED', error: I18n.t("discourse_fcm_notifications.subscribe_error") }
       end
     end
     
